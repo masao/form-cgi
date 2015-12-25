4bb108367948290ebb7e58ac956c9cef822b7c60
Masao Takaku <tmasao@acm.org>
2015-12-25 23:14:06 +0900

    admin.rb: accept to_csv param

--- admin.rb
+++ admin.rb
@@ -7,8 +7,14 @@ begin
    cgi = CGI.new
    formapp = FormCGIAdmin.new( cgi )
 
-   print cgi.header( "text/html; charset=utf-8" )
-   puts formapp.to_html
+   case cgi.value('format')
+   when "txt", "csv"
+     print cgi.header( "text/plain; charset=utf-8" )
+     puts formapp.to_csv
+   else
+     print cgi.header( "text/html; charset=utf-8" )
+     puts formapp.to_html
+   end
 rescue
    print "Content-Type: text/plain\r\n\r\n"
    puts "#$! (#{$!.class})"
